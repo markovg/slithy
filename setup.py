bb29e754958200f5fa8dd092c3aebcf6784791d8
emuller <devnull@localhost>
2009-09-21 21:43:09 +0200

    Added data files, .so, fonts, etc. to setup.py install

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from distutils.core import setup
 setup(name = "slithy", version = "20030706",
-      packages = ["slithy"])
+      packages = ["slithy"],
+      package_data={'slithy': ['*.so', 'fonts/*']})
