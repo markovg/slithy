49d6669aa5d05c001e7970ef4c00540948daf106
emuller <devnull@localhost>
2016-02-22 11:32:35 +0100

    Added test.py to debug the segfault issue

---
+++ test.py
@@ -0,0 +1,7 @@
+import Tkinter as Tk
+root = Tk.Tk()
+from slithy import dobj
+
+root.tk.call( 'load', '', 'Slsoggy' )
+#w = Tkinter.Widget.__init__(self, master, 'Slsoggy::soggy'
+#root.tk.call('package', 'require', 'Slsoggy')
